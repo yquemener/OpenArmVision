c0ebd04750ccad1bdb106e109518d1ef851d9235
Yves Quemener <yves.quemener@gmail.com>
2024-10-01 23:06:58 +0200

    Now with select and delete

--- visionAI/annotation_gui.py
+++ visionAI/annotation_gui.py
@@ -11,6 +11,8 @@ Features:
 7. All shapes (including points) have a temporary state while dragging
 8. Integrated controls for mode selection and clearing annotations
 9. Annotations remain correctly positioned when resizing the widget
+10. Selection tool to select and move existing annotations
+11. Delete tool to delete selected annotations
 
 Constraints and Considerations:
 1. Uses PyQt5 for the GUI
@@ -22,9 +24,11 @@ Constraints and Considerations:
 """
 
 from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QButtonGroup,
-                             QGraphicsView, QGraphicsScene, QGraphicsItem, QGraphicsPixmapItem)
-from PyQt5.QtGui import QPixmap, QPen, QColor, QPainter
+                             QGraphicsView, QGraphicsScene, QGraphicsItem, QGraphicsPixmapItem,
+                             QShortcut)
+from PyQt5.QtGui import QPixmap, QPen, QColor, QPainter, QKeySequence
 from PyQt5.QtCore import Qt, QPointF, QRectF, pyqtSignal
+from math import sqrt
 
 class AnnotationItem(QGraphicsItem):
     def __init__(self, item_type, data, color=Qt.blue):
@@ -82,6 +86,7 @@ class AnnotationWidget(QWidget):
         self.current_item = None
         self.mode = "point"
         self.drawing = False
+        self.selected_item = None
 
         self.init_ui()
 
@@ -101,9 +106,11 @@ class AnnotationWidget(QWidget):
         point_button = QPushButton("Point")
         line_button = QPushButton("Line")
         rectangle_button = QPushButton("Rectangle")
+        select_button = QPushButton("Select")
+        delete_button = QPushButton("Delete")
         clear_button = QPushButton("Clear")
 
-        for button in [point_button, line_button, rectangle_button]:
+        for button in [point_button, line_button, rectangle_button, select_button]:
             button.setCheckable(True)
             self.button_group.addButton(button)
 
@@ -112,27 +119,45 @@ class AnnotationWidget(QWidget):
         point_button.clicked.connect(lambda: self.set_mode("point"))
         line_button.clicked.connect(lambda: self.set_mode("line"))
         rectangle_button.clicked.connect(lambda: self.set_mode("rectangle"))
+        select_button.clicked.connect(lambda: self.set_mode("select"))
+        delete_button.clicked.connect(self.delete_selected_item)
         clear_button.clicked.connect(self.clear_annotations)
 
         button_layout.addWidget(point_button)
         button_layout.addWidget(line_button)
         button_layout.addWidget(rectangle_button)
+        button_layout.addWidget(select_button)
+        button_layout.addWidget(delete_button)
         button_layout.addWidget(clear_button)
         button_layout.addStretch()
 
         self.setMinimumSize(600, 400)
 
+        # Keyboard shortcuts
+        select_button.setShortcut(QKeySequence("S"))
+        delete_shortcut = QShortcut(QKeySequence(Qt.Key_Delete), self)
+        delete_shortcut.activated.connect(self.delete_selected_item)
+
+    def set_mode(self, mode):
+        self.mode = mode
+        if mode != "select" and self.selected_item:
+            self.selected_item.color = Qt.blue
+            self.selected_item.update()
+            self.selected_item = None
+
     def mousePressEvent(self, event):
         if self.view.viewport().rect().contains(event.pos()):
             scene_pos = self.view.mapToScene(event.pos())
             if event.button() == Qt.LeftButton:
-                self.drawing = True
-                if self.mode == "point":
-                    self.current_item = AnnotationItem("point", scene_pos, Qt.red)
-                elif self.mode in ["line", "rectangle"]:
-                    self.current_item = AnnotationItem(self.mode, [scene_pos, scene_pos], Qt.red)
-                self.scene.addItem(self.current_item)
-        print("Mouse Press Event:", scene_pos)
+                if self.mode == "select":
+                    self.select_nearest_item(scene_pos)
+                else:
+                    self.drawing = True
+                    if self.mode == "point":
+                        self.current_item = AnnotationItem("point", scene_pos, Qt.red)
+                    elif self.mode in ["line", "rectangle"]:
+                        self.current_item = AnnotationItem(self.mode, [scene_pos, scene_pos], Qt.red)
+                    self.scene.addItem(self.current_item)
 
     def mouseMoveEvent(self, event):
         if self.drawing and self.view.viewport().rect().contains(event.pos()):
@@ -143,7 +168,6 @@ class AnnotationWidget(QWidget):
                 self.current_item.data[1] = scene_pos
             self.current_item.update()
             self.scene.update()
-        print("Mouse Move Event:", scene_pos)
 
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.LeftButton and self.drawing:
@@ -158,10 +182,74 @@ class AnnotationWidget(QWidget):
             self.scene.update()
             self.current_item = None
             self.annotation_changed.emit(self.get_annotations())
-        print("Mouse Release Event:", scene_pos)
 
-    def set_mode(self, mode):
-        self.mode = mode
+    def select_nearest_item(self, pos):
+        min_distance = float('inf')
+        nearest_item = None
+        for item in self.scene.items():
+            if isinstance(item, AnnotationItem):
+                distance = self.distance_to_item(item, pos)
+                if distance < min_distance:
+                    min_distance = distance
+                    nearest_item = item
+
+        if self.selected_item:
+            self.selected_item.color = Qt.blue
+            self.selected_item.update()
+
+        if nearest_item:
+            self.selected_item = nearest_item
+            self.selected_item.color = Qt.red
+            self.selected_item.update()
+
+    def distance_to_item(self, item, pos):
+        if item.item_type == "point":
+            return sqrt((item.pos().x() - pos.x())**2 + (item.pos().y() - pos.y())**2)
+        elif item.item_type == "line":
+            return min(self.point_to_line_distance(pos, item.data[0], item.data[1]),
+                       self.point_to_point_distance(pos, item.data[0]),
+                       self.point_to_point_distance(pos, item.data[1]))
+        elif item.item_type == "rectangle":
+            rect = QRectF(item.data[0], item.data[1]).normalized()
+            if rect.contains(pos):
+                return 0
+            else:
+                return min(self.point_to_line_distance(pos, rect.topLeft(), rect.topRight()),
+                           self.point_to_line_distance(pos, rect.topRight(), rect.bottomRight()),
+                           self.point_to_line_distance(pos, rect.bottomRight(), rect.bottomLeft()),
+                           self.point_to_line_distance(pos, rect.bottomLeft(), rect.topLeft()))
+
+    def point_to_line_distance(self, p, a, b):
+        # Calculate the distance from point p to line segment ab
+        ax, ay = a.x(), a.y()
+        bx, by = b.x(), b.y()
+        px, py = p.x(), p.y()
+
+        # Calculate the length of the line segment
+        length = sqrt((bx - ax)**2 + (by - ay)**2)
+
+        # If the length is zero, return the distance to either point
+        if length == 0:
+            return sqrt((px - ax)**2 + (py - ay)**2)
+
+        # Calculate the projection of p onto the line
+        t = ((px - ax) * (bx - ax) + (py - ay) * (by - ay)) / (length**2)
+
+        # If t is outside [0, 1], the closest point is an endpoint
+        if t < 0:
+            return sqrt((px - ax)**2 + (py - ay)**2)
+        elif t > 1:
+            return sqrt((px - bx)**2 + (py - by)**2)
+
+        # Calculate the closest point on the line
+        closest_x = ax + t * (bx - ax)
+        closest_y = ay + t * (by - ay)
+
+        # Return the distance to the closest point
+        return sqrt((px - closest_x)**2 + (py - closest_y)**2)
+
+    def point_to_point_distance(self, p1, p2):
+        return sqrt((p1.x() - p2.x())**2 + (p1.y() - p2.y())**2)
 
     def clear_annotations(self):
         self.scene.clear()
@@ -210,6 +298,12 @@ class AnnotationWidget(QWidget):
                 elif item.item_type in ["line", "rectangle"]:
                     annotations.append((item.item_type, item.data[0].x(), item.data[0].y(), item.data[1].x(), item.data[1].y()))
         return annotations
+
+    def delete_selected_item(self):
+        if self.selected_item:
+            self.scene.removeItem(self.selected_item)
+            self.selected_item = None
+            self.annotation_changed.emit(self.get_annotations())
 
 if __name__ == "__main__":
     from PyQt5.QtWidgets import QApplication, QMainWindow
